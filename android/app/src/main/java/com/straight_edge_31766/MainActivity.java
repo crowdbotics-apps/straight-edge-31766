875c174b0b48c40097a2a57b54b3fd2051b6bbce
Crowdbotics <team@crowdbotics.com>
2021-10-31 09:01:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/straight_edge_31766/MainActivity.java
@@ -0,0 +1,15 @@
+package com.straight_edge_31766;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "straight_edge_31766";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "straight_edge_31766",
+  url: "https://straight_edge_31766.botics.co",
+  api: "https://straight_edge_31766.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
